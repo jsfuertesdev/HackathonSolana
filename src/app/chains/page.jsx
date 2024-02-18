acdb2d23114163874b12adc8eae98e66c4a69d2d
Amappola <anam.porrasp@gmail.com>
2024-02-18 06:34:59 -0500

    Fixing bugs

--- src/app/chains/page.jsx
+++ src/app/chains/page.jsx
@@ -1,7 +1,4 @@
-import Card from "../components/card/card"
 import Dashboard from "../components/dashboard/Dashboard"
-import Navbar from "../components/navbar/Navbar"
-
 
 function page() {
   return (
