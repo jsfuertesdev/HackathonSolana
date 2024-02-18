8dcebd0e4b577e788b3cc2659324b544faeb5dfc
Amappola <anam.porrasp@gmail.com>
2024-02-18 10:04:53 -0500

    Client perspective ready

--- src/app/exercises-gallery/components/card/Card.jsx
+++ src/app/exercises-gallery/components/card/Card.jsx
@@ -1,17 +1,19 @@
 import './card.scss';
 
-function Card({id,title,author,description,exercise, tags}) {
+function Card({title, author, exercise, tags}) {
   return (
     <div>
-      <div className='card-general'>
-        <div className='container-img'>
+      <div className='card__container'>
+        <div className='card-image__container'>
           <img src={exercise} alt="exercise" />
         </div>
-        <h2>{title}</h2>
-        <p>{author}</p>
+        <div className="card-content__container">
+          <h2>{title}</h2>
+          <p>{author}</p>
+        </div>
         <div className='tags-container'>
           {tags.map((tag)=>(
-            <div className='tag'>{tag}</div>
+            <button className='tag btn'>{tag}</button>
           ))}
         </div>
       </div>
