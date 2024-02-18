bdd99b18109e76a033b14cb22fd9b1a1108fe5ba
Amappola <anam.porrasp@gmail.com>
2024-02-18 10:25:27 -0500

    Creating preview

---
+++ src/app/upload-exercise/components/exercise-preview/exercise-preview.jsx
@@ -0,0 +1,24 @@
+import './exercise-preview.scss';
+
+function Register({title, author, exercise, tags}) {
+  return (
+    <div>
+      <div className='card__container'>
+        <div className='card-image__container'>
+          <img src={exercise} alt="exercise" />
+        </div>
+        <div className="card-content__container">
+          <h2>{title}</h2>
+          <p>{author}</p>
+        </div>
+        <div className='tags-container'>
+          {tags.map((tag)=>(
+            <button className='tag btn'>{tag}</button>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
