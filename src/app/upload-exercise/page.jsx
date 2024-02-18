247de0cae8a5716220270fa20fe80724377715d3
Amappola <anam.porrasp@gmail.com>
2024-02-18 10:19:55 -0500

    Creating upload exercise space

--- src/app/upload-exercise/page.jsx
+++ src/app/upload-exercise/page.jsx
@@ -1,7 +1,13 @@
+import './page.scss';
 
 function page() {
   return (
-    <div>
+    <div className="upload-gallery__page">
+      <div className="title">
+        <h2>Upload</h2>
+      </div>
+      <div className="upload-exercise__form">
+      </div>
     </div>
   )
 }
